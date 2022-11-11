6a58b28425327a8637144a1db96108f4fbbed5c4
Ch1coMalo <105455288+Ch1coMalo@users.noreply.github.com>
2022-11-11 22:51:15 +0300

    Add files via upload

---
+++ ConsoleApp8/ConsoleApp8/ApplicationContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+using System.IO;
+
+
+
+namespace EfCoreTest1
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Reception> Receptions { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("jsconfig1.json");
+            var config = builder.Build();
+            string connectionString = config.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
---
+++ ConsoleApp8/ConsoleApp8/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfCoreTest1
+{
+    public class Reception     {
+        public int ReceptionID { get; set; }
+        public DateTime DataRec { get; set; }
+        public DateTime DataFinal { get; set; }
+        public int DiagnosisID { get; set; }
+        public int DocID { get; set; }
+        public int PacID { get; set; }
+        public int NumRecipe { get; set; }
+        public int NumAnalysis { get; set; }
+    }
+}
